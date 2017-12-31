11f0d1b6c0f06a05ea41b364a574c7618a5c6e88
lob <lob@f-it.de>
2017-12-31 19:04:41 +0100

    Add files via upload

---
+++ admin/words.js
@@ -0,0 +1,30 @@
+// Dictionary (systemDictionary is global variable from adapter-settings.js)
+systemDictionary = {
+	"PJLink adapter settings": {"de": "PJLink Adapter Einstellungen"
+															"ru": "Настройки адаптера PJLink"},
+
+	"Projector":							 {"de": "Projektor"
+															"ru": "проектор"},
+
+	"IP Address":              {"de": "IP Addresse"
+															"ru": "IP-адрес"},
+
+	"TCP Port":                {"de": "TCP Port"
+															"ru": "Порт TCP"},
+
+	"Password":                {"de": "Passwort"
+															"ru": "пароль"},
+
+	"Poll Interval":					 {"de": "Abfrage Intervall"
+															"ru": "Интервал запроса"},
+
+	"Protocol":								 {"de": "Protokoll"
+															"ru": "протокол"},
+
+	"Adapter":								 {"de": "Adapter"
+															"ru": "адаптер"},
+
+	"on save adapter restarts with new config immediately":
+														 {"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
+															"ru": "При сохранении настроек адаптера он сразу же перезапускается"}
+};

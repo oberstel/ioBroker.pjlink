99e7b2b14b857093b1beaa846506721019c14ad4
lob <lob@f-it.de>
2018-01-02 00:47:33 +0100

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -1,30 +1,47 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"PJLink adapter settings": {"de": "PJLink Adapter Einstellungen"
-															"ru": "Настройки адаптера PJLink"},
+	"PJLink adapter settings": {	
+		"de": "PJLink Adapter Einstellungen",
+		"ru": "Настройки адаптера PJLink"
+		},
 
-	"Projector":							 {"de": "Projektor"
-															"ru": "проектор"},
+	"Projector": {
+		"de": "Projektor",
+		"ru": "проектор"
+		},
 
-	"IP Address":              {"de": "IP Addresse"
-															"ru": "IP-адрес"},
+	"IP Address": {
+		"de": "IP Addresse",
+		"ru": "IP-адрес"
+		},
 
-	"TCP Port":                {"de": "TCP Port"
-															"ru": "Порт TCP"},
+	"TCP Port": {
+		"de": "TCP Port",
+		"ru": "Порт TCP"
+		},
 
-	"Password":                {"de": "Passwort"
-															"ru": "пароль"},
+	"Password": {
+		"de": "Passwort",
+		"ru": "пароль"
+		},
 
-	"Poll Interval":					 {"de": "Abfrage Intervall"
-															"ru": "Интервал запроса"},
+	"Poll Interval": {
+		"de": "Abfrage Intervall",
+		"ru": "Интервал запроса"
+		},
 
-	"Protocol":								 {"de": "Protokoll"
-															"ru": "протокол"},
+	"Protocol": {
+		"de": "Protokoll",
+		"ru": "протокол"
+		},
 
-	"Adapter":								 {"de": "Adapter"
-															"ru": "адаптер"},
+	"Adapter": {
+		"de": "Adapter",
+		"ru": "адаптер"
+		},
 
-	"on save adapter restarts with new config immediately":
-														 {"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
-															"ru": "При сохранении настроек адаптера он сразу же перезапускается"}
+	"on save adapter restarts with new config immediately": {
+		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
+		"ru": "При сохранении настроек адаптера он сразу же перезапускается"
+		}
 };
